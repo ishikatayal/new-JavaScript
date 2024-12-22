b73826ecec022dfc2686e2792c8ee2d3947bb4cb
ishikatayal <108132728+ishikatayal@users.noreply.github.com>
2024-12-22 09:23:58 +0000

    datatypes

---
+++ basics_1/variable_01.js
@@ -0,0 +1,12 @@
+const accountId = 11245
+let accountEmail = "ishika@gmail.com"
+var accountPassword = "479"
+accountCity = "roorkee"
+
+// accoutId = 5
+accountEmail = "tayal@gmail.com"
+accountPassword = "38697"
+accountCity = "chg"
+let accountState;
+console.log(accountId);
+console.table([accountId,accountEmail, accountPassword, accountCity, accountState])

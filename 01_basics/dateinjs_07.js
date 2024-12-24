719d3c37fbfd08d6316aa88fda6672d673ff9e90
ishikatayal <108132728+ishikatayal@users.noreply.github.com>
2024-12-24 07:52:23 +0000

    basics

---
+++ 01_basics/dateinjs_07.js
@@ -0,0 +1,15 @@
+let myDate= new Date()
+// console.log(myDate.toJSON());
+// console.log(myDate.toString());
+// console.log(myDate.toTimeString());
+// console.log(typeof myDate)
+let MyCreatedDate = new Date(2018,4,2)
+// console.log(MyCreatedDate.toDateString())
+let TimeStamp= Date.now()
+console.log(TimeStamp)
+console.log(MyCreatedDate.getTime())
+
+let NewDate= new Date()
+console.log(NewDate)
+console.log(NewDate.getMonth())
+
